8571dcf3ebd7900447c005c40a4563494abb12aa
kitzi66 <kitzi66@gmail.com>
2016-06-18 21:22:30 -0500

    Initial Commit

---
+++ HamburgesasDelMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburgesasDelMundo
+//
+//  Created by Kitzia Abarca Alfonso on 18/06/16.
+//  Copyright © 2016 KAA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
